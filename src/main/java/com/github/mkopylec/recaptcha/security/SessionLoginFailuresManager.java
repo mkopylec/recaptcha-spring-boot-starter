1c05704adc6a5fd18e6efc1550917b1f0729cb32
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2015-11-23 01:13:19 +0100

    refactor

--- src/main/java/com/github/mkopylec/recaptcha/security/SessionLoginFailuresManager.java
+++ src/main/java/com/github/mkopylec/recaptcha/security/SessionLoginFailuresManager.java
@@ -1,6 +1,7 @@
 package com.github.mkopylec.recaptcha.security;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 public class SessionLoginFailuresManager implements LoginFailuresManager {
 
@@ -14,21 +15,25 @@ public class SessionLoginFailuresManager implements LoginFailuresManager {
 
     @Override
     public void addLoginFailure() {
-        Object loginFailuresCount = request.getSession().getAttribute(LOGIN_FAILURES_COUNT_SESSION_ATTRIBUTE);
+        Object loginFailuresCount = getSession().getAttribute(LOGIN_FAILURES_COUNT_SESSION_ATTRIBUTE);
         int newFailuresCount = 1;
         if (loginFailuresCount != null) {
             newFailuresCount = ((int) loginFailuresCount) + 1;
         }
-        request.getSession().setAttribute(LOGIN_FAILURES_COUNT_SESSION_ATTRIBUTE, newFailuresCount);
+        getSession().setAttribute(LOGIN_FAILURES_COUNT_SESSION_ATTRIBUTE, newFailuresCount);
     }
 
     @Override
     public int getLoginFailuresCount() {
-        return (int) request.getSession().getAttribute(LOGIN_FAILURES_COUNT_SESSION_ATTRIBUTE);
+        return (int) getSession().getAttribute(LOGIN_FAILURES_COUNT_SESSION_ATTRIBUTE);
     }
 
     @Override
     public void resetLoginFailures() {
-        request.getSession().removeAttribute(LOGIN_FAILURES_COUNT_SESSION_ATTRIBUTE);
+        getSession().removeAttribute(LOGIN_FAILURES_COUNT_SESSION_ATTRIBUTE);
+    }
+
+    private HttpSession getSession() {
+        return request.getSession();
     }
 }
