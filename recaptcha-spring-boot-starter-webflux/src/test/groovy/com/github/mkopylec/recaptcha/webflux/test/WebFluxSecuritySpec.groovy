d65677d16f88d0dad12c74f0e63999521ba733ae
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2019-01-03 15:44:23 +0100

    web test client auto configuration removed

--- recaptcha-spring-boot-starter-webflux/src/test/groovy/com/github/mkopylec/recaptcha/webflux/test/WebFluxSecuritySpec.groovy
+++ recaptcha-spring-boot-starter-webflux/src/test/groovy/com/github/mkopylec/recaptcha/webflux/test/WebFluxSecuritySpec.groovy
@@ -1,8 +1,6 @@
 package com.github.mkopylec.recaptcha.webflux.test
 
 import com.github.mkopylec.recaptcha.test.specification.SecuritySpec
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 
-@AutoConfigureWebTestClient
 class WebFluxSecuritySpec extends SecuritySpec {
 }
--- recaptcha-spring-boot-starter-webflux/src/test/groovy/com/github/mkopylec/recaptcha/webflux/test/WebFluxValidationSpec.groovy
+++ recaptcha-spring-boot-starter-webflux/src/test/groovy/com/github/mkopylec/recaptcha/webflux/test/WebFluxValidationSpec.groovy
@@ -1,8 +1,6 @@
 package com.github.mkopylec.recaptcha.webflux.test
 
 import com.github.mkopylec.recaptcha.test.specification.ValidationSpec
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 
-@AutoConfigureWebTestClient
 class WebFluxValidationSpec extends ValidationSpec {
 }
